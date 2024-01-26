79e1cd114526694c33921cbc8bf89e106e24eeee
Erick Namukolo <erickmndev@gmail.com>
2024-01-26 23:14:26 +0200

    added basic layout of the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import Logo from './components/logo';
+import Form from './components/form';
+import ParkingList from './components/packing-list';
+import Stats from './components/stats';
 
 function App() {
-  return <div className='App'>Hello world</div>;
+  return (
+    <div className='app'>
+      <Logo />
+      <Form />
+      <ParkingList />
+      <Stats />
+    </div>
+  );
 }
 
 export default App;
