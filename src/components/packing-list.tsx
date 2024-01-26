8d3cac479aafb7b2d3849619ceeff07bf55e18e0
Erick Namukolo <erickmndev@gmail.com>
2024-01-26 23:10:49 +0200

    added starter components

---
+++ src/components/packing-list.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ParkingList: React.FC = () => {
+  return <div className='list'>List</div>;
+};
+
+export default ParkingList;
---
+++ src/components/stats.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Stats: React.FC = () => {
+  return (
+    <footer className='add-Stats'>
+      <em>You have X items on your list, and you are already packed x (x%)</em>
+    </footer>
+  );
+};
+
+export default Stats;
