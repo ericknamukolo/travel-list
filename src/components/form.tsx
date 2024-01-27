d5f8bc29ce9436812d34c42802ab0dd3e9e88b9b
Erick Namukolo <erickmndev@gmail.com>
2024-01-27 17:18:02 +0200

    get values from form

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
 const Form: React.FC = () => {
+  function handleSubmit(e: any) {
+    e.preventDefault();
+    console.log(e.target[0].value);
+    console.log(e.target[1].value);
+  }
   return (
-    <form className='add-form'>
+    <form className='add-form' onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
       <select>
         {Array.from({ length: 20 }, (_, i) => (
